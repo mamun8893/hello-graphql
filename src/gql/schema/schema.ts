b38c7e7f91525448e0825d4bac5c09183610ac0b
mamun8893 <0316361936@student.wub.edu.bd>
2023-12-18 00:18:43 +0600

    categoy data

---
+++ src/gql/schema/schema.ts
@@ -0,0 +1,27 @@
+export const typeDefs = `#graphql
+
+  type Product {
+    id: ID!
+    name: String
+    image: String
+    description: String
+    price: Float
+    quantity:Int
+    onStock:Boolean
+    categoryId:String
+
+  }
+
+  type Category {
+    id:ID!
+    name:String
+  }
+
+
+  type Query {
+    products:[Product]
+    product(productId:ID!):Product
+    categories:[Category]
+    category(categoryId:ID!):Category
+  }
+`;
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { db } from "./db.js";
+import { resolvers } from "./gql/resolvers/resolvers.js";
+import { typeDefs } from "./gql/schema/schema.js";
 
-const typeDefs = `#graphql
-
-  type Product {
-    id: ID!
-    name: String
-    image: String
-    description: String
-    price: Float
-    quantity:Int
-    onStock:Boolean
-    categoryId:String
-
-  }
-
-
-  type Query {
-    products:[Product]
-    product(productId:ID!):Product
-  }
-`;
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    products: () => db.products,
-    product: (parent: any, args: { productId: string }, context: any) => {
-      const result = db.products.find(
-        (product) => product.id === args.productId
-      );
-      return result;
-    },
-  },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
