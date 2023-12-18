dedbd9f4141f1590d8f760e17add29763349bcaa
mamun8893 <0316361936@student.wub.edu.bd>
2023-12-18 23:27:48 +0600

    one to one relation

--- src/gql/resolvers/resolvers.ts
+++ src/gql/resolvers/resolvers.ts
@@ -17,4 +17,12 @@ export const resolvers = {
       return result;
     },
   },
+  Product: {
+    category: (parent, args, context) => {
+      const result = db.categories.find(
+        (category) => category.id === parent.categoryId
+      );
+      return result;
+    },
+  },
 };
